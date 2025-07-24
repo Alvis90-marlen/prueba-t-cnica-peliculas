e363544e718ca437c0508a2460beb6a0c0498f34
Alvis90-marlen <andres_fvalenciaa@soy.sena.edu.co>
2025-07-24 16:18:42 -0500

    Delete peliculas-frontend directory

--- peliculas-frontend/src/main.ts
+++
@@ -1,7 +0,0 @@
-    import { bootstrapApplication } from '@angular/platform-browser'; // Importa bootstrapApplication
-    import { appConfig } from './app/app.config'; // Importa tu configuración de la aplicación
-    import { AppComponent } from './app/app.component'; // Asegúrate de que AppComponent esté importado
-
-    bootstrapApplication(AppComponent, appConfig) // Usa bootstrapApplication
-      .catch((err) => console.error(err));
-    
